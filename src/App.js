124fca6d8dd2e43467f130db8f5f63596829c4ca
lucas mello <lucasmello03@hotmail.com>
2023-06-04 16:07:54 -0300

    iniciando o projeto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import { Notification, Section } from "react-bulma-companion";
 
-const App = () => <p>Javascript Notes</p>;
+const App = () => (
+  <div>
+    <Section>
+      <Notification color="success">Javascript Notes!</Notification>
+    </Section>
+  </div>
+);
 
 export default App;
